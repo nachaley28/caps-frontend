d28a9d15876e6afab6d498c3114fe694aa59f5b0
tonyo <arabit.tr@gmail.com>
2025-09-25 10:55:51 +0800

    new: added server configurations (host, port), and list of allowed hostnames

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+  const allowed_host_str = env.VITE_HOSTS_ALLOWED 
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: '../dist'
+    },
+    server: {
+      host: env.VITE_ADDRESS,
+      port: env.VITE_PORT,
+      allowedHosts: allowed_host_str.split(',').map(host => host.trim()).filter(host => host)
+    }
+  }
 })
