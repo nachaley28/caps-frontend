d2bad12f476040a998bb38534afaa606039e9840
tonyo <arabit.tr@gmail.com>
2025-09-25 11:20:13 +0800

    update: App.jsx setup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion'; 
+import { motion, AnimatePresence } from 'framer-motion';
 import './App.css';
 import LandingPage from "./pages/Landing";
 import AdminLayout from "./pages/admin/AdminLayout";
 import Dashboard from "./pages/admin/dashboard";
-import Laboratory from "./pages/admin/labs";  
-import Reports from "./pages/admin/reports";  
-import UserRoles from "./pages/admin/users";  
-
-
+import Laboratory from "./pages/admin/labs";
+import Reports from "./pages/admin/reports";
+import UserRoles from "./pages/admin/users";
 
 
 function RouteWithTransitions() {
@@ -19,32 +17,25 @@ function RouteWithTransitions() {
   return (
     <AnimatePresence>
       <motion.div
-        key={location.pathname}  
-        initial={{ opacity: 0 }} 
-        animate={{ opacity: 1 }}  
-        exit={{ opacity: 0 }}    
-        transition={{ duration: 1 }}  
-        className="page-container" 
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        className="page-container"
       >
         <Routes location={location}>
           <Route path="/" element={<LandingPage />} />
 
-    
-
-
           {/* Admin Routes */}
           <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Dashboard/>} />
+            <Route index element={<Dashboard />} />
             <Route path="/admin/labs" element={<Laboratory />} />
             <Route path="/admin/reports" element={<Reports />} />
             <Route path="/admin/users" element={<UserRoles />} />
+          </Route>
+        </Routes>
 
-
-
-            </Route>
-
-           </Routes>
-        
       </motion.div>
     </AnimatePresence>
   );
